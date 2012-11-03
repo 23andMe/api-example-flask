c9dcd35f15087f402b1e526d719d4bb30d10902e
Ashwin Purohit <purohit@gmail.com>
2012-11-03 01:44:02 -0700

    copyright removal

--- client.py
+++ client.py
@@ -1,14 +1,4 @@
 #!/usr/bin/python
-"""
-   Copyright 2012 23andMe, Inc.
-
-   Unless required by applicable law or agreed to in writing, software
-   distributed under the License is distributed on an "AS IS" BASIS,
-   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-   
-"""
-
-
 
 import webbrowser
 import requests
